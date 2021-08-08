44b52feef9668719d14b52e30f3c95777db8779b
Leon <sverdleon@gmail.com>
2021-08-08 18:15:48 +0300

    Change bevy from certain revision to main

--- crates/bevy_common_input/src/plugin.rs
+++ crates/bevy_common_input/src/plugin.rs
@@ -2,7 +2,7 @@ use super::{resources::*, systems::*};
 use bevy::prelude::*;
 pub struct PlayerInputPlugin;
 impl Plugin for PlayerInputPlugin {
-    fn build(&self, app: &mut AppBuilder) {
+    fn build(&self, app: &mut App) {
         app.insert_resource(MouseWorldPosition::default())
             .add_system(get_mouse_world_position.system());
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 pub use bevy_common_core::*;
-pub use bevy_common_ecs_tilemap::*;
+//pub use bevy_common_ecs_tilemap::*;
 pub use bevy_common_input::*;
-pub use bevy_common_map::*;
+//pub use bevy_common_map::*;
